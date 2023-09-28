69c38a0f49efe6c83353804c4a5640a6f9704636
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-09-28 11:42:22 -0600

    I am now able to display the product name

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -5,11 +5,14 @@ let productDataStorage = {}
 
 export default async function productDetails(productId, selector) {
     // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-    // once we have the product details we can render out the HTML
-    // add a listener to Add to Cart button
-
-    // Add to cart functions
     const product = await findProductById(productId);
+
+    // once we have the product details we can render out the HTML
+    const el = document.querySelector(selector);
+    el.insertAdjacentHTML("afterBegin", productDetailsTemplate(product));
+    
+    // add a listener to Add to Cart button
+    
 
     let cart = getLocalStorage("cart") || { products: [] };
 
@@ -22,14 +25,6 @@ export default async function productDetails(productId, selector) {
         
         addProductToCart(product);
     }
-
-    // function displayProduct(item) {
-    //     const el = document.querySelector(selector);
-    //     const html = item.map(productDetailsTemplate);
-    //     el.insertAdjacentHTML('afterbegin', html.join(''))
-    // }
-
-    // displayProduct(product)
     
 
     document
