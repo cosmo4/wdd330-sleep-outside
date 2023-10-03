8f360a419cdd23086b7de459bfb82eb821d32a3d
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-10-03 10:46:38 -0600

    added the trash icon. feel free to modify

--- src/js/cart.js
+++ src/js/cart.js
@@ -11,20 +11,24 @@ function renderCartContents() {
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  const newItem = 
+  `<div class="cart-card-container">
+    <li class="cart-card divider">
+      <a href="#" class="cart-card__image">
+        <img
+          src="${item.Image}"
+          alt="${item.Name}"
+        />
+      </a>
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+      <p class="cart-card__quantity">qty: 1</p>
+      <p class="cart-card__price">$${item.FinalPrice}</p>
+    </li>
+    <img class="delete-icon" src="/images/delete-bin-line.svg" alt="remove product"></img>
+  </div>`;
 
   return newItem;
 }
