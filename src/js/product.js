f27890e98f141c6f7ad26ca2b5903dc0b0861098
fkordell <far18009@byui.edu>
2023-11-02 02:58:26 -0600

    Duplicate cart is fixed/ updated superscript

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,10 @@
 import { getParam } from './utils.mjs';
-import productDetails from './productDetails.mjs';
+import productDetails, { updateSuperscript } from './productDetails.mjs';
 import { renderHeaderFooter } from './utils.mjs';
 
 const productId = getParam('product');
 productDetails(productId, '.product-detail');
 
 renderHeaderFooter();
+updateSuperscript();
+
